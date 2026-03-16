404bad1457424743a00e3b05432a8446a23259f3
Arno <arn.hovas@gmail.com>
2026-03-16 14:00:44 +0400

    Finish DOM manipulation

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ const Result = (() => {
       winner: null,
       roundText: null,
       winType: null,
+      winRow: null,
     };
   }
 
@@ -43,43 +44,47 @@ const Result = (() => {
 })();
 
 const Player = (() => {
-  function createPlayer(type) {
-    const player = {
-      type,
-      score: 0,
-    };
+  function CreatePlayer(type) {
+    this.type = type;
+    this.score = 0;
+    this.name = `Player ${this.type}`;
 
-    return player;
+    return this;
   }
 
-  return { createPlayer };
+  CreatePlayer.prototype.updateName = function (value) {
+    if (!value) return;
+
+    this.name = value;
+  };
+
+  return { CreatePlayer };
 })();
 
 const GameController = (() => {
-  const playerX = Player.createPlayer('X');
-  const playerO = Player.createPlayer('O');
-  const board = Board.getBoard();
+  const playerX = new Player.CreatePlayer('X');
+  const playerO = new Player.CreatePlayer('O');
   let currentPlayer = playerX;
-  const result = Result.getResult();
 
   function isWinnerFound() {
-    return result.winner !== null;
+    return Result.getResult().winner !== null;
   }
 
   function toggleTurn() {
     currentPlayer = currentPlayer.type === 'X' ? playerO : playerX;
   }
 
-  function updateWinnerResult(player, type) {
-    result.winner = player;
-    result.roundText = `${player.type} winner!`;
-    result.winType = type;
+  function updateWinnerResult(player, type, winIndex) {
+    Result.getResult().winner = player;
+    Result.getResult().roundText = `${player.name} winner!`;
+    Result.getResult().winType = type;
+    Result.getResult().winIndex = winIndex;
   }
 
   function updateDrawResult() {
-    result.winner = null;
-    result.roundText = `It's a tie!`;
-    result.winType = 'Tie';
+    Result.getResult().winner = null;
+    Result.getResult().roundText = `It's a tie!`;
+    Result.getResult().winType = 'Tie';
   }
 
   function updateWinnerScore(player) {
@@ -87,13 +92,17 @@ const GameController = (() => {
   }
 
   function isBoardFull() {
-    return board.flat().every((col) => col !== '') && !isWinnerFound();
+    return (
+      Board.getBoard()
+        .flat()
+        .every((col) => col !== '') && !isWinnerFound()
+    );
   }
 
   function makeMove(row, col) {
-    if (board[row][col] || isWinnerFound()) return;
+    if (Board.getBoard()[row][col] || isWinnerFound()) return;
 
-    board[row][col] = currentPlayer.type;
+    Board.getBoard()[row][col] = currentPlayer.type;
     const gameOver = checkWin(currentPlayer.type);
 
     if (!gameOver) toggleTurn();
@@ -105,16 +114,32 @@ const GameController = (() => {
     currentPlayer = playerX;
   }
 
-  function checkWin(move) {
-    const isDiagonalWin =
-      (board[0][0] === move && board[1][1] === move && board[2][2] === move) ||
-      (board[0][2] === move && board[1][1] === move && board[2][0] === move);
+  function checkDiagonalWin(move) {
+    const board = Board.getBoard();
+
+    const diagonalDownLeft =
+      board[0][0] === move && board[1][1] === move && board[2][2] === move;
+    const diagonalUpRight =
+      board[0][2] === move && board[1][1] === move && board[2][0] === move;
+    const isDiagonalWin = diagonalDownLeft || diagonalUpRight;
 
     if (isDiagonalWin) {
-      updateWinnerResult(currentPlayer, 'diagonal');
+      const diagonalWinType = diagonalDownLeft
+        ? 'diagonal-down-left'
+        : 'diagonal-up-right';
+      updateWinnerResult(currentPlayer, diagonalWinType);
       updateWinnerScore(currentPlayer);
+
       return true;
     }
+
+    return false;
+  }
+
+  function checkWin(move) {
+    const board = Board.getBoard();
+
+    if (checkDiagonalWin(move)) return true;
 
     for (let i = 0; i < 3; i++) {
       const isHorizontalWin = board[i].every((c) => c === move);
@@ -122,13 +147,13 @@ const GameController = (() => {
         board[0][i] === move && board[1][i] === move && board[2][i] === move;
 
       if (isHorizontalWin) {
-        updateWinnerResult(currentPlayer, 'horizontal');
+        updateWinnerResult(currentPlayer, 'horizontal', i);
         updateWinnerScore(currentPlayer);
         return true;
       }
 
       if (isVerticalWin) {
-        updateWinnerResult(currentPlayer, 'vertical');
+        updateWinnerResult(currentPlayer, 'vertical', i);
         updateWinnerScore(currentPlayer);
         return true;
       }
@@ -142,5 +167,156 @@ const GameController = (() => {
     return false;
   }
 
-  return { makeMove, restartRound };
+  function getCurrentPlayer() {
+    return currentPlayer;
+  }
+
+  function getPlayerX() {
+    return playerX;
+  }
+
+  function getPlayerO() {
+    return playerO;
+  }
+
+  return {
+    makeMove,
+    restartRound,
+    getCurrentPlayer,
+    getPlayerX,
+    getPlayerO,
+  };
 })();
+
+// Display Controller
+
+const DisplayController = (() => {
+  const resultEl = document.querySelector('.result');
+  const playerXScore = document.getElementById('score-x');
+  const playerOScore = document.getElementById('score-o');
+  const playerTurnEl = document.getElementById('player-turn');
+  const boardBtns = document.querySelectorAll('.board-column');
+  const newRoundBtn = document.getElementById('new-round');
+  const playerXInput = document.getElementById('playerX');
+  const playerOInput = document.getElementById('playerO');
+
+  const playerX = GameController.getPlayerX();
+  const playerO = GameController.getPlayerO();
+
+  function renderBoard() {
+    const board = Board.getBoard();
+
+    boardBtns.forEach((boardBtn) => {
+      const row = Number(boardBtn.dataset.row);
+      const column = Number(boardBtn.dataset.col);
+
+      boardBtn.textContent = board[row][column];
+    });
+  }
+
+  function styleHorizontalVerticalCols(type, index) {
+    const columns =
+      type === 'vertical'
+        ? document.querySelectorAll(`button[data-col='${index}'`)
+        : document.querySelectorAll(`button[data-row='${index}'`);
+
+    columns.forEach((col) => col.classList.add('win'));
+  }
+
+  function styleDiagonalsCols(type) {
+    if (type === 'diagonal-down-left') {
+      for (let i = 0; i < 3; i++) {
+        document
+          .querySelector(`button[data-row='${i}'][data-col='${i}']`)
+          .classList.add('win');
+      }
+    } else {
+      let col = 0;
+      for (let row = 2; row >= 0; row--) {
+        document
+          .querySelector(`button[data-row='${row}'][data-col='${col}']`)
+          .classList.add('win');
+        col++;
+      }
+    }
+  }
+
+  function styleWin(type, index) {
+    switch (type) {
+      case 'horizontal':
+        styleHorizontalVerticalCols(type, index);
+        return;
+      case 'vertical':
+        styleHorizontalVerticalCols(type, index);
+        return;
+
+      case 'diagonal-down-left':
+        styleDiagonalsCols(type);
+        return;
+
+      case 'diagonal-up-right':
+        styleDiagonalsCols(type);
+        return;
+
+      default:
+        return;
+    }
+  }
+
+  function updateScoreDisplay() {
+    playerXScore.textContent = playerX.score;
+    playerOScore.textContent = playerO.score;
+  }
+
+  function updateFinishDisplay(text) {
+    resultEl.textContent = text;
+  }
+
+  function gameInit() {
+    boardBtns.forEach((boardBtn) => {
+      boardBtn.addEventListener('click', (e) => {
+        const row = Number(e.target.dataset.row);
+        const column = Number(e.target.dataset.col);
+
+        GameController.makeMove(row, column);
+        const winType = Result.getResult().winType;
+
+        if (winType !== null) {
+          const winIndex = Result.getResult().winIndex;
+          updateFinishDisplay(Result.getResult().roundText);
+          updateScoreDisplay();
+          styleWin(winType, winIndex);
+        }
+
+        const currentPlayer = GameController.getCurrentPlayer();
+        updatePlayerTurnDisplay(currentPlayer);
+        renderBoard();
+      });
+    });
+    // Change players name
+    playerXInput.addEventListener('input', (e) => {
+      playerX.updateName(e.target.value);
+    });
+    playerOInput.addEventListener('input', (e) => {
+      playerO.updateName(e.target.value);
+    });
+
+    newRoundBtn.addEventListener('click', handleRestart);
+  }
+
+  function handleRestart() {
+    GameController.restartRound();
+    renderBoard();
+    updatePlayerTurnDisplay(GameController.getCurrentPlayer());
+    resultEl.textContent = '';
+    boardBtns.forEach((boardBtn) => boardBtn.classList.remove('win'));
+  }
+
+  function updatePlayerTurnDisplay(currentPlayer) {
+    playerTurnEl.textContent = `${currentPlayer.type} turn`;
+  }
+
+  return { gameInit };
+})();
+
+DisplayController.gameInit();
